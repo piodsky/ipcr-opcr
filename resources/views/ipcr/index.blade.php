68060f870568bacdd0003b8f85f9391796e3a90a
unknown <piodskybaby@gmail.com>
2017-04-04 10:44:26 +0800

    change in route ipcr.index

--- resources/views/ipcr/index.blade.php
+++ resources/views/ipcr/index.blade.php
@@ -34,7 +34,7 @@
                 <div class="col-sm-6 col-md-3">
                     <div class="card">
                         <div class="card-block">
-                            <a href="{{route('notes.createNote',$ipcr->id)}}">
+                            <a href="/ipcr/{{$ipcr->id}}">
                                 <h4 class="card-title">
                                     {{$ipcr->name}}
                                 </h4>
