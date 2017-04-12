8062d5f9c0f686ee4f36437386a9ba627f5b7d38
unknown <piodskybaby@gmail.com>
2017-04-12 11:30:22 +0800

    change in frontpage

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-12 col-md-offset-0">
             <div class="panel panel-default">
-                <div class="panel-heading"><img src="dist/img/a.png" alt="bsu" height="150" width="1100 class="bsu1></div>
+                <div class="panel-heading"><img src="dist/img/logo11.png" alt="bsu" height="150" width="1100 class="bsu1></div>
 
                 <div class="panel-body">
 
